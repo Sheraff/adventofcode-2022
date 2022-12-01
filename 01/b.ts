2652d243ae71aeb78cf7ec92dabcd28b525c8a0f
Sheraff <fpellet@ensc.fr>
2022-12-01 13:45:57 +0100

    async b

--- 01/b.ts
+++ 01/b.ts
@@ -1,22 +1,27 @@
-import { readFile } from "node:fs/promises"
+import * as readline from 'node:readline/promises'
 import { dirname, join } from "node:path"
+import { createReadStream } from 'node:fs'
 
 const cwd = new URL(dirname(import.meta.url)).pathname
-const input = await readFile(join(cwd, "input-a.txt"), {encoding: "utf-8"})
-const lines = input.split("\n")
+const rl = readline.createInterface({
+	input: createReadStream(join(cwd, "input-a.txt"), { encoding: "utf-8" })
+})
 
-const calories = [0]
-let index = 0
-for (const line of lines) {
-	if (!line) {
-		index++
-		calories[index] = 0
-		continue
-	} else {
-		const cals = Number(line)
-		calories[index] += cals
-		continue
+void async function () {
+	const calories = [0]
+	let index = 0
+	for await (const line of rl) {
+		if (!line) {
+			index++
+			calories[index] = 0
+			continue
+		} else {
+			const cals = Number(line)
+			calories[index] += cals
+			continue
+		}
 	}
-}
-calories.sort((a, b) => b - a)
-console.log(calories[0] + calories[1] + calories[2])
+	calories.sort((a, b) => b - a)
+	console.log(calories[0] + calories[1] + calories[2])
+}()
+
