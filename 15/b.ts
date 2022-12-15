f212b5e3a5dca70d1e5c0ce7b6bfb10a39cfcb25
Sheraff <fpellet@ensc.fr>
2022-12-15 21:28:14 +0100

    day #15 (in a reasonable amount of time)

--- 15/b.ts
+++ 15/b.ts
@@ -5,8 +5,7 @@ const cwd = new URL(dirname(import.meta.url)).pathname
 const input = await readFile(join(cwd, "input-a.txt"), {encoding: "utf-8"})
 const lines = input.split("\n")
 
-const MIN = [0, 0] as const
-const MAX = [4_000_000, 4_000_000] as const
+const MAX = 4_000_000
 
 function tune(x: number, y: number) {
 	return x * 4_000_000 + y
@@ -21,43 +20,97 @@ for (const line of lines) {
 	pairs.push([x, y, bx, by, distance])
 }
 
-let cell: undefined | [number, number]
-findCell: for (let ROW_Y = MIN[1]; ROW_Y <= MAX[1]; ROW_Y++) {
-	console.log(`row ${ROW_Y}`)
-	const row = new Array(MAX[0] - MIN[0] + 1).fill(0) as number[]
-
-	for (const [x, y, , , d] of pairs) {
-		if (y - d > ROW_Y) continue
-		if (y + d < ROW_Y) continue
-
-		const startX = Math.max(MIN[0], x - d)
-		const endX = Math.min(MAX[0], x + d)
-
-		for (let i = startX; i <= endX; i++) {
-			if (row[i] === 1) continue
-			const cellDistance = Math.abs(i - x) + Math.abs(ROW_Y - y)
-			if (cellDistance <= d) {
-				row[i] = 1
-			}
+const ALL_RANGES = new Map<number, [number, number][]>()
+for (const [x, y, bx, by, d] of pairs) {
+	for (let i = Math.max(0, y - d); i <= Math.min(MAX, y + d); i++) {
+		const minX = x - (d - Math.abs(i - y))
+		const maxX = x + (d - Math.abs(i - y))
+		if (maxX < minX) continue
+		const newRange: [number, number] = [Math.max(0, minX), Math.min(MAX, maxX)]
+		const rowRanges = ALL_RANGES.get(i)
+		if (!rowRanges) {
+			ALL_RANGES.set(i, [newRange])
+			continue
 		}
+		ALL_RANGES.set(i, [...rowRanges, newRange])
 	}
-
-	for (const [, , bx, by] of pairs) {
-		if (by !== ROW_Y) continue
-		const i = bx - MIN[0]
-		row[i] = 0
-	}
-
-	for (let x = 0; x < row.length; x++) {
-		if (row[x] === 0) {
-			cell = [x, ROW_Y]
-			break findCell
-		}
+}
+for (const [x, y, bx, by, d] of pairs) {
+	if (by < 0 || by > MAX) continue
+	if (bx < 0 || bx > MAX) continue
+	const rowRanges = ALL_RANGES.get(by)
+	if (!rowRanges) {
+		ALL_RANGES.set(by, [[bx, bx]])
+	} else {
+		ALL_RANGES.set(by, [...rowRanges, [bx, bx]])
 	}
 }
 
-if (!cell) throw new Error(`no cell found`)
-console.log({cell})
+for (let y = 0; y < MAX; y++) {
+	let rowRanges = ALL_RANGES.get(y)
+	if (!rowRanges) throw new Error(`no ranges for row ${y}`)
+	const merged = mergeRanges(rowRanges)
 
-const frequency = tune(...cell)
-console.log({frequency})
+	if (merged.length === 0 || merged.length > 2) throw new Error(`row ${y} has ${merged.length} ranges`)
+	if (merged.length === 1) {
+		continue
+	}
+
+	const [[amin, amax], [bmin, bmax]] = merged as [[number, number], [number, number]]
+	let min
+	let max
+	if (amax < bmin) {
+		min = amax
+		max = bmin
+	} else if (bmax < amin) {
+		min = bmax
+		max = amin
+	} else {
+		throw new Error(`row ${y} has overlapping ranges ${amin}-${amax} and ${bmin}-${bmax}`)
+	}
+	const delta = max - min
+	if (delta !== 2) throw new Error(`row ${y} has ${delta} empty tiles between ${amin}-${amax} and ${bmin}-${bmax}`)
+
+	const x = min + 1
+	const result = tune(x, y)
+	console.log(`result: ${result}`)
+}
+
+
+function mergeRanges(ranges: [number, number][]) {
+	let merged: [number, number][] = []
+	for (const [minX, maxX] of ranges) {
+		if (merged.length === 0) {
+			merged.push([minX, maxX])
+			continue
+		}
+
+		const contains = merged.some(([min, max]) => minX >= min && maxX <= max)
+		if (contains) continue
+
+		const overlaps = merged.find(([min, max]) =>
+			(minX >= min - 1 && minX <= max + 1)
+			|| (maxX >= min - 1 && maxX <= max + 1)
+			|| (min >= minX - 1 && min <= maxX + 1)
+			|| (max >= minX - 1 && max <= maxX + 1)
+		)
+		if (overlaps) {
+			overlaps[0] = Math.min(overlaps[0], minX)
+			overlaps[1] = Math.max(overlaps[1], maxX)
+
+			let length = merged.length
+			let changed = true
+			while (changed && length > 1) {
+				merged = mergeRanges(merged)
+				changed = merged.length !== length
+				length = merged.length
+			}
+
+			continue
+		}
+
+		merged.push([minX, maxX])
+	}
+
+	return merged
+}
