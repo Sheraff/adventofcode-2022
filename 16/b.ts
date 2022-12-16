53b4c0545f2253a33916994b746d3766ed405d3e
Sheraff <fpellet@ensc.fr>
2022-12-16 20:39:03 +0100

    day #16 part 2

--- 16/b.ts
+++ 16/b.ts
@@ -5,112 +5,211 @@ const cwd = new URL(dirname(import.meta.url)).pathname
 const input = await readFile(join(cwd, "input-a.txt"), {encoding: "utf-8"})
 const lines = input.split("\n")
 
-const MAX = 4_000_000
+const REGEX = /^Valve ([A-Z]+) has flow rate=([0-9]+); tunnels? leads? to valves? (.*)$/
 
-function tune(x: number, y: number) {
-	return x * 4_000_000 + y
+type Valve = {
+	name: string
+	flow: number
+	leadsTo: [string, number][]
 }
 
-const REGEX = /Sensor at x=([\-0-9]+), y=([\-0-9]+): closest beacon is at x=([\-0-9]+), y=([\-0-9]+)/
+const TIME_LIMIT = 26
 
-const pairs: [number, number, number, number, number][]	= []
+const graph = new Map<string, Valve>()
+
+// make graph
 for (const line of lines) {
-	const [, x, y, bx, by] = line.match(REGEX)!.map(Number) as [number, number, number, number, number]
-	const distance = Math.abs(x - bx) + Math.abs(y - by)
-	pairs.push([x, y, bx, by, distance])
+	const [, name, flow, list] = line.match(REGEX)! as [string, string, string, string]
+	const leadsTo = list.split(", ").map(name => [name, 1] as [string, number])
+	graph.set(name, {
+		name,
+		flow: Number(flow),
+		leadsTo,
+	})
 }
 
-const ALL_RANGES = new Map<number, [number, number][]>()
-for (const [x, y, bx, by, d] of pairs) {
-	for (let i = Math.max(0, y - d); i <= Math.min(MAX, y + d); i++) {
-		const minX = x - (d - Math.abs(i - y))
-		const maxX = x + (d - Math.abs(i - y))
-		if (maxX < minX) continue
-		const newRange: [number, number] = [Math.max(0, minX), Math.min(MAX, maxX)]
-		const rowRanges = ALL_RANGES.get(i)
-		if (!rowRanges) {
-			ALL_RANGES.set(i, [newRange])
-			continue
-		}
-		ALL_RANGES.set(i, [...rowRanges, newRange])
-	}
-}
-for (const [x, y, bx, by, d] of pairs) {
-	if (by < 0 || by > MAX) continue
-	if (bx < 0 || bx > MAX) continue
-	const rowRanges = ALL_RANGES.get(by)
-	if (!rowRanges) {
-		ALL_RANGES.set(by, [[bx, bx]])
-	} else {
-		ALL_RANGES.set(by, [...rowRanges, [bx, bx]])
-	}
-}
-
-for (let y = 0; y < MAX; y++) {
-	let rowRanges = ALL_RANGES.get(y)
-	if (!rowRanges) throw new Error(`no ranges for row ${y}`)
-	const merged = mergeRanges(rowRanges)
-
-	if (merged.length === 0 || merged.length > 2) throw new Error(`row ${y} has ${merged.length} ranges`)
-	if (merged.length === 1) {
+// compress graph
+for (const [name, valve] of graph) {
+	if (name === 'AA') {
 		continue
 	}
-
-	const [[amin, amax], [bmin, bmax]] = merged as [[number, number], [number, number]]
-	let min
-	let max
-	if (amax < bmin) {
-		min = amax
-		max = bmin
-	} else if (bmax < amin) {
-		min = bmax
-		max = amin
-	} else {
-		throw new Error(`row ${y} has overlapping ranges ${amin}-${amax} and ${bmin}-${bmax}`)
+	if (valve.flow === 0) {
+		valve.leadsTo.forEach(([leadName, leadDuration]) => {
+			const leadValve = graph.get(leadName)!
+			const additionalLeadsTo = valve.leadsTo
+				.filter(([name]) => name !== leadName && leadValve.leadsTo.every(([otherName]) => otherName !== name))
+				.map(([name, duration]) => [name, duration + leadDuration] as [string, number])
+			leadValve.leadsTo.forEach(([name, duration], i) => {
+				const removed = valve.leadsTo.find(([otherName]) => otherName === name)
+				if (removed) {
+					leadValve.leadsTo[i] = [name, Math.min(duration, removed[1] + leadDuration)]
+				}
+			})
+			leadValve.leadsTo = leadValve.leadsTo
+				.filter(([name]) => name !== valve.name)
+				.concat(additionalLeadsTo)
+		})
+		graph.delete(name)
 	}
-	const delta = max - min
-	if (delta !== 2) throw new Error(`row ${y} has ${delta} empty tiles between ${amin}-${amax} and ${bmin}-${bmax}`)
-
-	const x = min + 1
-	const result = tune(x, y)
-	console.log(`result: ${result}`)
 }
 
+console.log(graph)
 
-function mergeRanges(ranges: [number, number][]) {
-	let merged: [number, number][] = []
-	for (const [minX, maxX] of ranges) {
-		if (merged.length === 0) {
-			merged.push([minX, maxX])
-			continue
+// memoize all paths from any node to any node (or write a function that finds path and memoizes it) and associated travel duration
+const paths = new Map<string, Map<string, number>>()
+function findPathFromTo(from: string, to: string, avoid: Set<string> = new Set()) {
+	let fromMap = paths.get(from)
+	if (fromMap) {
+		const duration = fromMap.get(to)
+		if (duration !== undefined) {
+			return duration
 		}
-
-		const contains = merged.some(([min, max]) => minX >= min && maxX <= max)
-		if (contains) continue
-
-		const overlaps = merged.find(([min, max]) =>
-			(minX >= min - 1 && minX <= max + 1)
-			|| (maxX >= min - 1 && maxX <= max + 1)
-			|| (min >= minX - 1 && min <= maxX + 1)
-			|| (max >= minX - 1 && max <= maxX + 1)
-		)
-		if (overlaps) {
-			overlaps[0] = Math.min(overlaps[0], minX)
-			overlaps[1] = Math.max(overlaps[1], maxX)
-
-			let length = merged.length
-			let changed = true
-			while (changed && length > 1) {
-				merged = mergeRanges(merged)
-				changed = merged.length !== length
-				length = merged.length
-			}
-
-			continue
-		}
-
-		merged.push([minX, maxX])
+	}
+	if (!fromMap) {
+		fromMap = new Map()
+		paths.set(from, fromMap)
 	}
 
-	return merged
+	let duration: number
+
+	const valve = graph.get(from)!
+	const found = valve.leadsTo.find(([name]) => name === to)
+	if (found) {
+		duration = found[1]
+	} else {
+		duration = valve.leadsTo
+			.filter(([name]) => !avoid.has(name))
+			.map(([name, duration]) => {
+				const subAvoid = new Set(avoid)
+				subAvoid.add(from)
+				return duration + findPathFromTo(name, to, subAvoid)
+			})
+			.reduce((a, b) => Math.min(a, b), Infinity)
+	}
+
+	if (avoid.size === 0) {
+		fromMap.set(to, duration)
+		if (!paths.has(to)) {
+			paths.set(to, new Map())
+		}
+		paths.get(to)!.set(from, duration)
+	}
+
+	return duration
+}
+
+// create set of all closed valves (all valves - valves with flow === 0)
+const closedValves = new Set(graph.keys())
+closedValves.delete('AA')
+let maxSoFar = 0
+const score = processGraph([['AA'],['AA']], closedValves, 0)
+console.log({score})
+
+// AA > JJ > BB > CC
+// AA > DD > HH > EE
+function processGraph(agents: string[][], closed: Set<string>, start: number = 1): number {
+	for (let minute = start; minute <= TIME_LIMIT; minute++) {
+		for (const agent of agents) {
+			if (agent.at(-2) && agent.at(-1) !== agent.at(-2)) {
+				agent.push(agent.at(-1)!)
+				closed.delete(agent.at(-1)!)
+			}
+		}
+		const availableAgents = agents.filter(agent => agent.length === minute)
+		if (!availableAgents.length) {
+			continue
+		}
+		if (availableAgents.length === 2) {
+			const current = availableAgents.map(agent => agent.at(-1)!)
+			const possibilities = Array.from(closed.values())
+				.filter(name => !current.includes(name))
+				.map(name => [name, current.map(c => findPathFromTo(c, name))] as [string, number[]])
+				.filter(([,durations]) => durations.some(
+					(duration, i) => duration + minute + (graph.get(current[i]!)!.flow === 0 ? 0 : 1) < TIME_LIMIT)
+				)
+
+			if (!possibilities.length) {
+				continue
+			}
+			
+			const permutations: [string, string][] = []
+			for (let i = 0; i < possibilities.length; i++) {
+				if (possibilities[i]![1][0]! + minute + graph.get(current[0]!)!.flow === 0 ? 0 : 1 >= TIME_LIMIT) continue
+				for (let j = 0; j < possibilities.length; j++) {
+					if (i === j) continue
+					if (possibilities[j]![1][1]! + minute + graph.get(current[1]!)!.flow === 0 ? 0 : 1 >= TIME_LIMIT) continue
+					permutations.push([possibilities[i]![0], possibilities[j]![0]])
+				}
+			}
+
+			if (!permutations.length) {
+				continue
+			}
+
+			const scores: number[] = []
+			for (const [a, b] of permutations) {
+				const aArray = new Array(findPathFromTo(current[0]!, a) - 1).fill(current[0]).concat(a)
+				const bArray = new Array(findPathFromTo(current[1]!, b) - 1).fill(current[1]).concat(b)
+				scores.push(processGraph([
+					[...agents[0]!, ...aArray],
+					[...agents[1]!, ...bArray],
+				], new Set(closed), minute))
+			}
+			return Math.max(...scores)
+		}
+		if (availableAgents.length === 1) {
+			const agent = availableAgents[0]!
+			const current = agent.at(-1)!
+			const otherCurrents = agents.filter(other => other !== agent).map(other => other.at(-1)!)
+			const possibilities = Array.from(closed.values())
+				.filter(name => name !== current && !otherCurrents.includes(name))
+				.map(name => [name, findPathFromTo(current, name)] as [string, number])
+				.filter(([,duration]) => minute + duration + (graph.get(current)!.flow === 0 ? 0 : 1) < TIME_LIMIT)
+
+			if (!possibilities.length) {
+				continue
+			}
+
+			const scores: number[] = []
+			for (const possibility of possibilities) {
+				const array = new Array(possibility[1] - 1).fill(current).concat(possibility[0])
+				const nextAgents = agents.map(other => other === agent ? [...agent, ...array] : other)
+				scores.push(processGraph(nextAgents, new Set(closed), minute))
+			}
+			return Math.max(...scores)
+		}
+	}
+
+	// resolve
+	return resolve(agents)
+}
+
+function resolve(agents: string[][]): number {
+	const open: Set<string> = new Set()
+	let releasePerMinute = 0
+	let score = 0
+	for (let minute = 1; minute < TIME_LIMIT; minute++) {
+		for (const agent of agents) {
+			const index = Math.min(minute, agent.length - 1)
+			if (agent[index]! === agent[index-1]!) {
+				if (!open.has(agent[index]!)) {
+					releasePerMinute += graph.get(agent[index]!)!.flow
+					open.add(agent[index]!)
+				}
+			}
+		}
+		score += releasePerMinute
+	}
+	display(agents, score)
+	return score
+}
+
+function display(agents: string[][], score: number) {
+	if (maxSoFar < score) {
+		maxSoFar = score
+		const string = agents.map(agent => Array.from(new Set(agent)).join(" > ")).join('\n')
+		console.log()
+		console.log(string)
+		console.log(`${score} / ${maxSoFar}`)
+	}
 }
