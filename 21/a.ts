499e43e0335f57cdc0be9ca62b54cc30ab533d1a
Sheraff <fpellet@ensc.fr>
2022-12-21 09:10:22 +0100

    day #21

---
+++ 21/a.ts
@@ -0,0 +1,63 @@
+import {readFile} from "node:fs/promises"
+import { dirname, join } from "node:path"
+
+const cwd = new URL(dirname(import.meta.url)).pathname
+const input = await readFile(join(cwd, "input-a.txt"), {encoding: "utf-8"})
+
+type Monkey = {
+	name: string
+} & (
+	| {
+		number: number
+	}
+	| {
+		a: string
+		b: string
+		op: '+' | '-' | '*' | '/'
+	}
+)
+
+const REGEX = /^([a-z]{4})\: (?:(\d+)|(?:([a-z]{4}) ([\+\-\/\*]) ([a-z]{4})))$/
+
+const set = new Map<string, Monkey>(input.split("\n").map(l => {
+	const match = l.match(REGEX)!
+	if (match[2]) {
+		return [match[1]!, {
+			name: match[1]!,
+			number: Number(match[2]!),
+		}]
+	} else {
+		return [match[1]!, {
+			name: match[1]!,
+			a: match[3]!,
+			b: match[5]!,
+			op: match[4]! as '+' | '-' | '*' | '/',
+		}]
+	}
+}))
+
+console.log(set.get('root'))
+
+console.log(solveNode(set.get('root')!))
+
+function solveNode(node: Monkey): number {
+	if ('number' in node) {
+		return node.number
+	} else {
+		const a = solveNode(set.get(node.a)!)
+		const b = solveNode(set.get(node.b)!)
+		let number
+		switch (node.op) {
+			case '+': number = a + b; break
+			case '-': number = a - b; break
+			case '*': number = a * b; break
+			case '/': number = a / b; break
+		}
+		set.set(node.name, {
+			name: node.name,
+			number,
+		})
+		return number
+	}
+}
+
